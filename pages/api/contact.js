2a05bd7ac9436abab0dc0599c4dda61af9a4c054
helloalexdodd <hello@alexdodd.ca>
2020-09-06 17:22:05 -0400

    updating form api

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -4,7 +4,6 @@ const mailer = require('../../mailer');
 export default (req, res) => {
   if (req.method === 'POST') {
     const { error } = joiValidator(req.body);
-    console.log(error);
     if (error) return res.status(404).send(error.details[0].message);
 
     const { name, email, message } = req.body;
--- server.js
+++ server.js
@@ -5,24 +5,9 @@ const app = next({ dev });
 const handle = app.getRequestHandler();
 const PORT = process.env.PORT || 4000;
 
-const { joiValidator, validate } = require('./formValidation');
-const mailer = require('./mailer');
-
 app.prepare().then(() => {
   const server = express();
   server.use(express.json());
-
-  server.post('/api/contact', validate(joiValidator), (req, res) => {
-    const { name, email, message } = req.body;
-
-    mailer({ email, name, text: message })
-      .then(() => {
-        res.send({ msg: 'success' });
-      })
-      .catch((err) => {
-        res.send(err);
-      });
-  });
 
   server.get('*', (req, res) => {
     return handle(req, res);
