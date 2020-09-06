7279bd4b7324845f3b9cfe1f6692998a55abede7
helloalexdodd <hello@alexdodd.ca>
2020-09-06 19:18:33 -0400

    adding validation

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,4 +1,10 @@
+const { joiValidator } = require('../../formValidation.js');
+const mailer = require('../../mailer');
+
 export default (req, res) => {
+  const { error } = joiValidator(req.body);
+  if (error) return res.status(404).send(error.details[0].message);
+
   const { name, email, message } = req.body;
 
   mailer({ email, name, text: message })
